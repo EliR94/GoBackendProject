78b364008a05620beae7acfdd80109cfa1841efd
Elizabeth Ray <elizabeth-94@hotmail.co.uk>
2025-10-21 13:34:34 +0100

    GREET-4 - Set Up GitHub Actions

--- main.go
+++ main.go
@@ -2,14 +2,8 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	port := "3000"
-	r := gin.Default()
-	fmt.Println("Starting API on port " + port)
-	err := r.Run(":" + port)
-	fmt.Println(err)
+	fmt.Println("Hello World")
 }
