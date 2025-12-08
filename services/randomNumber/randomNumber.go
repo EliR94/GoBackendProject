9ed977c263068c8953ae05a1f97b49956f8c0162
Elizabeth Ray <elizabeth-94@hotmail.co.uk>
2025-12-08 15:31:38 +0000

    GREET-8/CreateGETRandomGreeting: update comment

--- services/randomNumber/randomNumber.go
+++ services/randomNumber/randomNumber.go
@@ -14,7 +14,7 @@ type RandomNumberService interface {
 type RealRandomNumberService struct {
 }
 
-// this function provides the randomNumber service with a NewRandomNumber method and enables the stucture on line ? to become a RandomNumberService
+// this function provides the randomNumber service with a NewRandomNumber method and enables the stucture on line 14 to become a RandomNumberService
 func (r *RealRandomNumberService) NewRandomNumber(maxNumber int) int {
 	randomNumber, _ := rand.Int(rand.Reader, big.NewInt(int64(maxNumber)))
 	return int(randomNumber.Int64())
